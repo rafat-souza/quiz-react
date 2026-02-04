e737ca2b68c3566ea08571a8d8790cc648cf46be
Rafael Toledo Souza <ch1at@outlook.com>
2026-02-03 23:24:15 -0300

    context

---
+++ src/context/quiz.jsx
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+
+export const QuizContext = createContext();
+
+export const QuizProvider = ({ children }) => {
+  const value = {};
+
+  return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
+};

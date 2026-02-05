c92c1f758f8cf0217cbc9d5914ece8f0cfec82b6
Rafael Toledo Souza <ch1at@outlook.com>
2026-02-05 10:35:07 -0300

    alterando estado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { useContext } from "react";
+
 import Welcome from "./components/Welcome";
+import Question from "./components/Question";
 
 import "./App.css";
+import { QuizContext } from "./context/quiz";
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
   return (
     <div className="App">
       <h1>Quiz de Programação</h1>
-      <Welcome />
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Playing" && <Question />}
     </div>
   );
 }

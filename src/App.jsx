d76c08a696e1418c8b40dbd780c3aa3c74981ef9
Rafael Toledo Souza <ch1at@outlook.com>
2026-02-08 17:51:27 -0300

    componente de categoria

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import { QuizContext } from "./context/quiz";
 
 import Welcome from "./components/Welcome";
+import PickCategory from "./components/PickCategory";
 import Question from "./components/Question";
 import GameOver from "./components/GameOver";
 
@@ -18,6 +19,7 @@ function App() {
     <div className="App">
       <h1>Quiz de Programação</h1>
       {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Category" && <PickCategory />}
       {quizState.gameStage === "Playing" && <Question />}
       {quizState.gameStage === "End" && <GameOver />}
     </div>

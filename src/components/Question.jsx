45c89a67cae6bb4cd8c03a3c7cf30952e6c96511
Rafael Toledo Souza <ch1at@outlook.com>
2026-02-08 21:48:05 -0300

    ajuda de eliminar opção

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -29,6 +29,7 @@ const Question = () => {
             key={option}
             answer={currentQuestion.answer}
             selectOption={() => onSelectOption(option)}
+            hide={quizState.optionToHide === option ? "hide" : null}
           />
         ))}
       </div>
@@ -37,6 +38,9 @@ const Question = () => {
           {currentQuestion.tip && (
             <button onClick={() => dispatch({ type: "SHOW_TIP" })}>Dica</button>
           )}
+          <button onClick={() => dispatch({ type: "REMOVE_OPTION" })}>
+            Excluir uma
+          </button>
         </>
       )}
       {!quizState.answerSelected && quizState.help === "tip" && (
